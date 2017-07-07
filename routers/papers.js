de64af89b05ec6599151ea851c590612e4d383e8
Adolfo Reyna <aeroreyna@gmail.com>
2017-07-06 23:54:15 -0700

    fetch from ieee and elsevier new schema

--- routers/papers.js
+++ routers/papers.js
@@ -23,12 +23,13 @@ router.get('/name/:AlgorithmName', function (req, res) {
   if ( req.params.AlgorithmName){
     list.findOne({title:req.params.AlgorithmName}, function(err, data){
       if(data){
-        papers.find({AlgorithmName:req.params.AlgorithmName}).limit(100).toArray((err, docs)=>{
+        papers.find({algorithmname:req.params.AlgorithmName}).limit(100).toArray((err, docs)=>{
           if(docs.length != 0){
             res.json( docs );
           }else{
             res.send("We don't have this algorithm information, try again in 2 minutes.")
             fetchFromIEEE(req.params.AlgorithmName);
+            fetchFromElsevier(req.params.AlgorithmName);
           }
         });
       }
@@ -57,14 +58,14 @@ router.get('/count', function (req, res) {
   var agregateArray = [];
   agregateArray.push({$group:
     {
-      _id: {AlgorithmName:"$AlgorithmName", py :"$py"},
+      _id: {algorithmname:"$algorithmname", py :"$year"},
       count: { $sum: 1 }
     }
   });
   agregateArray.push({$group:
     {
-      _id: {AlgorithmName:"$_id.AlgorithmName"},
-      countYears: {$push: { year:"$_id.py", count:"$count"  }},
+      _id: {algorithmname:"$_id.algorithmname"},
+      countYears: {$push: { year:"$_id.year", count:"$count"  }},
       count: { $sum: "$count" }
     }
   });
@@ -82,14 +83,15 @@ let fetchFromIEEE = function(AlgorithmName) {
   let totalFound = 0;
   let urlApi = "http://ieeexplore.ieee.org/gateway/ipsSearch.jsp";
   let sortParams = "&sortfield=py&sortorder=desc";
-  let allPapers = [];
 
-  axios.get(urlApi + "?ab=" + AlgorithmName + "&hc=1")
+  axios.get(urlApi + '?ab="' + AlgorithmName + '"&hc=1')
     .then(response => {
       parseString(response.data, (err, result)=>{
-        totalFound = result.root.totalfound;
-        console.log("Number of Articles Found: ", totalFound);
-        getNextBatch();
+        if(!result.Error){
+          totalFound = result.root.totalfound;
+          console.log("IEEE, Number of Articles Found: ", totalFound);
+          getNextBatch();
+        }
       })
     }).catch(error => {
       console.log(error);
@@ -97,21 +99,92 @@ let fetchFromIEEE = function(AlgorithmName) {
 
   let getNextBatch = function(){
     rs = NoArtPerPage * pageNumber + 1;
-    axios.get(urlApi + "?ab=" + AlgorithmName + "&rs=" + rs + sortParams + "&hc=" + NoArtPerPage)
+    console.log("Loading Page:", pageNumber)
+    axios.get(urlApi + '?ab="' + AlgorithmName + '"&rs=' + rs + sortParams + "&hc=" + NoArtPerPage)
       .then(response => {
+        var papersR = [];
         parseString(response.data, (err, result)=>{
           if (err) console.log(err);
           result.root.document.forEach( a =>{
-            a.AlgorithmName = AlgorithmName;
+            var data = {};
+            data.title = a.title.trim();
+            data.authors = a.authors.split(";");
+            data.year = a.py;
+            data.pubtitle = a.pubtitle;
+            data.link = a.mdurl;
+            data.algorithmname = AlgorithmName;
+            data.provider = "IEEE"
+            papersR.push(data);
           });
-          papers.insertMany(result.root.document, (err, r)=>{
-            console.log((NoArtPerPage * pageNumber + result.root.document.length) + " papers were succesfully added", AlgorithmName);
+          papers.insertMany(papersR, (err, r)=>{
+            console.log((NoArtPerPage * pageNumber + papersR.length) + " papers were succesfully added", AlgorithmName);
             pageNumber += 1
             if (NoArtPerPage * pageNumber + 1< totalFound){
               getNextBatch();
+            }else{
+              console.log("IEEE Fetch succesfull");
             }
           });
         })
+      }).catch(error => {
+        console.log(error);
+      });
+  }
+}
+
+let fetchFromElsevier = function(AlgorithmName) {
+  let webRes = "";
+  let NoArtPerPage = 200; //default
+  let pageNumber = 0;
+  let rs = NoArtPerPage * pageNumber;
+  let totalFound = 0;
+  let apiKey = process.env.elsevierKey;
+  let urlApi = "http://api.elsevier.com/content/search/scidir?apiKey="+apiKey+"&httpAccept=application/json&content=journals";
+  let sortParams = "&sort=-coverDate";
+
+
+  axios.get(urlApi + '&query=%22' + AlgorithmName + '%22&count=1')
+    .then(response => {
+      totalFound = parseInt(response.data["search-results"]["opensearch:totalResults"]);
+      console.log("Elsevier, Number of Articles Found: ", totalFound);
+      if(totalFound) getNextBatch();
+    }).catch(error => {
+      console.log(error);
+    });
+
+  let getNextBatch = function(){
+    rs = NoArtPerPage * pageNumber;
+    console.log("Loading Page:", pageNumber)
+    axios.get(urlApi + '&query=%22' + AlgorithmName + '%22&count=' + NoArtPerPage+ '&start=' + rs + sortParams)
+      .then(response => {
+        var papersR = [];
+        response.data["search-results"]["entry"].forEach( a =>{
+          var data = {};
+          data.title = a["dc:title"].trim();
+          var authors = [];
+          if(a.authors){
+            a.authors.author.forEach(au => {
+              authors.push(au["given-name"] + " " + au["surname"]);
+            })
+          }
+          data.authors = authors;
+          var date = a["prism:coverDisplayDate"];
+          data.year = date.substring(date.length-4);
+          data.pubtitle = a["prism:publicationName"];
+          data.link = a.link[1]["@href"];
+          data.algorithmname = AlgorithmName;
+          data.provider = "Elsevier"
+          papersR.push(data);
+        });
+        papers.insertMany(papersR, (err, r)=>{
+          console.log((NoArtPerPage * pageNumber + papersR.length) + " papers were succesfully added", AlgorithmName);
+          pageNumber += 1
+          if (NoArtPerPage * pageNumber < totalFound){
+            getNextBatch();
+          }else{
+            console.log("Elsevier Fetch succesfull");
+          }
+        });
       }).catch(error => {
         console.log(error);
       });
